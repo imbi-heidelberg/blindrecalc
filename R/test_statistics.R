34413ddf4cf5908aa840517e6b4300432d6fddb1
Maximilian Pilz <38661816+MatheMax@users.noreply.github.com>
2019-12-17 10:08:14 +0100

    setup

---
+++ R/test_statistics.R
@@ -0,0 +1,72 @@
+#' Test statistics
+#'
+#' In \code{blindrecalc}, different test statistics are implemented.
+#' Currently, those are Student's t-test for superiority and non-inferiorty
+#' for continuous outcomes, the Chi^2-test for superiority tests for binary
+#' outcomes and the Farrington Manning test for non-inferiority tests for
+#' binary outcomes.
+#'
+#' @export
+setClass("TestStatistic", representation(
+  alpha    = "numeric",
+  beta     = "numeric",
+  r        = "numeric",
+  delta    = "numeric",
+  delta_NI = "numeric",
+  n_max    = "numeric"
+))
+
+setClass("Student", contains = "TestStatistic")
+
+setClass("ChiSquare", contains = "TestStatistic")
+
+setClass("FarringtonManning", contains = "TestStatistic")
+
+
+#' Setup Student's t-test
+#'
+#' This function creates an object of class \code{Student} that can
+#' be used for sample size recalculation.
+#'
+#' @template setup
+#'
+#' @export
+setupStudent <- function(alpha, beta, r = 1, delta, delta_NI = 0, n_max = Inf) {
+  new("Student", alpha = alpha, beta = beta, r = r, delta = delta,
+      delta_NI = delta_NI, n_max = n_max)
+}
+
+
+
+#' Setup a Chi^2 test
+#'
+#' This function creates an object of class \code{ChiSquare} that can
+#' be used for sample size recalculation.
+#'
+#' @template setup
+#'
+#' @details For non-inferiority trials use the function \code{\link{setupFarringtionManning}}.
+#'
+#' @export
+setupChiSquare <- function(alpha, beta, r = 1, delta, delta_NI = 0, n_max = Inf) {
+  new("ChiSquare", alpha = alpha, beta = beta, r = r, delta = delta,
+      delta_NI = delta_NI, n_max = n_max)
+}
+
+
+
+
+#' Setup a Farrington Manning test
+#'
+#' This function creates an object of class \code{FarringtonManning} that can
+#' be used for sample size recalculation in non-inferiority trials with
+#' binary endpoints.
+#'
+#' @template setup
+#'
+#' @export
+setupFarringtonManning <- function(alpha, beta, r = 1, delta, delta_NI = 0, n_max = Inf) {
+  new("FarringtonManning", alpha = alpha, beta = beta, r = r, delta = delta,
+      delta_NI = delta_NI, n_max = n_max)
+}
+
---
+++ man-roxygen/setup.R
@@ -0,0 +1,6 @@
+#' @param alpha one-sided type I error rate
+#' @param beta type II error rate
+#' @param r allocation ratio between experimental and control group
+#' @param delta difference of effect size between alternative and null hypothesis
+#' @param delta_NI non-inferiority margin
+#' @param n_max maximal overall sample size
