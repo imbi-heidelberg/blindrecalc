06e304d28a7df8fe4f6424d9b9e99e21daca91ab
Maximilian Pilz <38661816+MatheMax@users.noreply.github.com>
2020-02-13 09:14:06 +0100

    moderate speed up

--- R/Student.R
+++ R/Student.R
@@ -17,20 +17,20 @@
 #'
 #' @export
 simulation <- function(design, n1, nuisance, recalculation = TRUE, Delta_star, iters = 1000, ...) {
-            r <- design@r
+            alloc <- design@r / (1 + design@r)^2
 
             # Step 1
             z1 <- stats::rnorm(n = iters, mean = 0, sd = 1)
             v1 <- stats::rchisq(n = iters, df = n1 - 2)
 
             # Step 2
-            var_hat <- nuisance^2 / (n1 - 1) * (v1 + (z1 + sqrt(r * n1 / (1 + r)^2) * Delta_star / nuisance)^2)
+            var_hat <- nuisance^2 / (n1 - 1) * (v1 + (z1 + sqrt(n1 * alloc) * Delta_star / nuisance)^2)
 
             # Step 3
             if (recalculation == FALSE) {
               n <- n1
               } else {
-                n_recalc <- (1 + r)^2 / r * (stats::qnorm(1 - design@alpha) + stats::qnorm(1 - design@beta))^2 /
+                n_recalc <- 1 / alloc * (stats::qnorm(1 - design@alpha) + stats::qnorm(1 - design@beta))^2 /
                   (design@delta - design@delta_NI)^2 * var_hat
                 n        <- sapply(n_recalc, function(m) min(design@n_max, max(ceiling(m), n1)))
               }
@@ -39,7 +39,7 @@ simulation <- function(design, n1, nuisance, recalculation = TRUE, Delta_star, i
             f <- function(i) {
               # Step 4
               if(n2[i] == 0) {
-                test_statistic <- (z1[i] + sqrt(n1 * r / (1 + r)^2) * (Delta_star - design@delta_NI) / nuisance) / sqrt(v1[i] / (n1 - 2))
+                test_statistic <- (z1[i] + sqrt(n1 * alloc) * (Delta_star - design@delta_NI) / nuisance) / sqrt(v1[i] / (n1 - 2))
                 } else {
                   # Step 5
                   z2 <- stats::rnorm(n = 1, mean = 0, sd = 1)
@@ -47,7 +47,7 @@ simulation <- function(design, n1, nuisance, recalculation = TRUE, Delta_star, i
                   v2 <- w2 + (sqrt(n2[i] / n[i]) * z1[i] - sqrt(n1 / n[i]) * z2)^2
 
                   test_statistic <-
-                    (sqrt(n1 / n[i]) * z1[i] + sqrt(n2[i] / n[i]) * z2 + sqrt(n[i] * r / (1 + r)^2) *
+                    (sqrt(n1 / n[i]) * z1[i] + sqrt(n2[i] / n[i]) * z2 + sqrt(n[i] * alloc) *
                        (Delta_star - design@delta_NI) / nuisance) / sqrt((v1[i] + v2) / (n[i] - 2))
                   }
               return(test_statistic)
