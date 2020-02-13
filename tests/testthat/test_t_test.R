5775e90b1ec385d51bd854f537128fe1ece92659
Maximilian Pilz <38661816+MatheMax@users.noreply.github.com>
2020-02-13 09:32:44 +0100

    ...

--- tests/testthat/test_t_test.R
+++ tests/testthat/test_t_test.R
@@ -1,7 +1,7 @@
 context("test t-test")
 
 design <- setupStudent(alpha = .025, beta = .2, r = 1, delta = 3.5,
-                       delta_NI =  0, n_max = Inf)
+                       delta_NI = 0, n_max = Inf)
 
 test_that("Example of Lu can be reproduced", {
   its <- 1e5
