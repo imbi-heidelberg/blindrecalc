c65cb7b698ed8f1eefb17403ddc41010f0747f0f
Maximilian Pilz <38661816+MatheMax@users.noreply.github.com>
2020-02-13 09:08:45 +0100

    student works for unbalanced design r!=1

--- R/Student.R
+++ R/Student.R
@@ -17,20 +17,21 @@
 #'
 #' @export
 simulation <- function(design, n1, nuisance, recalculation = TRUE, Delta_star, iters = 1000, ...) {
-            if (design@r != 1) stop("the unbalanced case is not implemented yet!")
+            r <- design@r
 
             # Step 1
             z1 <- stats::rnorm(n = iters, mean = 0, sd = 1)
-            v1 <- stats::rchisq(n = iters, df = 2 * n1 - 2)
+            v1 <- stats::rchisq(n = iters, df = n1 - 2)
 
             # Step 2
-            var_hat <- nuisance^2 / (2 * n1 - 1) * (v1 + (z1 + sqrt(n1 / 2) * Delta_star / nuisance)^2)
+            var_hat <- nuisance^2 / (n1 - 1) * (v1 + (z1 + sqrt(r * n1 / (1 + r)^2) * Delta_star / nuisance)^2)
 
             # Step 3
             if (recalculation == FALSE) {
               n <- n1
               } else {
-                n_recalc <- 2 * (stats::qnorm(1 - design@alpha) + stats::qnorm(1 - design@beta))^2 / (design@delta - design@delta_NI)^2 * var_hat
+                n_recalc <- (1 + r)^2 / r * (stats::qnorm(1 - design@alpha) + stats::qnorm(1 - design@beta))^2 /
+                  (design@delta - design@delta_NI)^2 * var_hat
                 n        <- sapply(n_recalc, function(m) min(design@n_max, max(ceiling(m), n1)))
               }
             n2 <- n - n1
@@ -38,23 +39,23 @@ simulation <- function(design, n1, nuisance, recalculation = TRUE, Delta_star, i
             f <- function(i) {
               # Step 4
               if(n2[i] == 0) {
-                test_statistic <- (z1[i] + sqrt(n1 / 2) * (Delta_star - design@delta_NI) / nuisance) / sqrt(v1[i] / (2 * n1 - 2))
+                test_statistic <- (z1[i] + sqrt(n1 * r / (1 + r)^2) * (Delta_star - design@delta_NI) / nuisance) / sqrt(v1[i] / (n1 - 2))
                 } else {
                   # Step 5
                   z2 <- stats::rnorm(n = 1, mean = 0, sd = 1)
-                  w2 <- stats::rchisq(n = 1, df = 2 * n2[i] - 1)
+                  w2 <- stats::rchisq(n = 1, df = n2[i] - 1)
                   v2 <- w2 + (sqrt(n2[i] / n[i]) * z1[i] - sqrt(n1 / n[i]) * z2)^2
 
                   test_statistic <-
-                    (sqrt(n1 / n[i]) * z1[i] + sqrt(n2[i] / n[i]) * z2 + sqrt(n[i] / 2) *
-                       (Delta_star - design@delta_NI) / nuisance) / sqrt((v1[i] + v2) / (2 * n[i] - 2))
+                    (sqrt(n1 / n[i]) * z1[i] + sqrt(n2[i] / n[i]) * z2 + sqrt(n[i] * r / (1 + r)^2) *
+                       (Delta_star - design@delta_NI) / nuisance) / sqrt((v1[i] + v2) / (n[i] - 2))
                   }
               return(test_statistic)
             }
 
             test_statistic <- sapply(seq(1, iters, 1), f)
 
-            critical_value <- stats::qt(1 - design@alpha, df = 2 * n - 2)
+            critical_value <- stats::qt(1 - design@alpha, df = n - 2)
             reject <- ifelse(test_statistic >= critical_value, 1, 0)
 
             return(list(
@@ -91,6 +92,4 @@ setMethod("n_fix", signature("Student"),
             (1 + design@r) * 2 * (stats::qnorm(1 - design@alpha) + stats::qnorm(1 - design@beta))^2 /
               (design@delta - design@delta_NI)^2 * nuisance^2
           })
-
-
 
--- tests/testthat/test_t_test.R
+++ tests/testthat/test_t_test.R
@@ -1,18 +1,19 @@
 context("test t-test")
 
-design <- setupStudent(.025, .2, 1, 3.5, 0, Inf)
+design <- setupStudent(alpha = .025, beta = .2, r = 1, delta = 3.5,
+                       delta_NI =  0, n_max = Inf)
 
 test_that("Example of Lu can be reproduced", {
   its <- 1e5
 
   expect_equal(
-    toer(design, 20, 5.5, TRUE, its),
+    toer(design, 40, 5.5, TRUE, its),
     design@alpha,
     tolerance = 2 / sqrt(its), scale = 1)
 
 
   expect_equal(
-    pow(design, 20, 5.5, TRUE, its),
+    pow(design, 40, 5.5, TRUE, its),
     1 - design@beta,
     tolerance = 2 / sqrt(its), scale = 1)
 
