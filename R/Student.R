8259bae0e5bfbfa127621bbd21f6f7ff8d8293f8
Maximilian Pilz <38661816+MatheMax@users.noreply.github.com>
2020-02-17 14:47:45 +0100

    spacing

--- R/Student.R
+++ R/Student.R
@@ -17,52 +17,53 @@
 #' Pharmaceutical Statistics 15: 208-215.
 #'
 #' @export
-simulation <- function(design, n1, nuisance, recalculation = TRUE, Delta_star, iters = 1000, ...) {
-            alloc <- design@r / (1 + design@r)^2
+simulation <- function(design, n1, nuisance, recalculation = TRUE, Delta_star, iters = 1000, seed = NULL, ...) {
+  if (!is.null(seed)) set.seed(seed)
 
-            # Step 1
-            z1 <- stats::rnorm(n = iters, mean = 0, sd = 1)
-            v1 <- stats::rchisq(n = iters, df = n1 - 2)
+  alloc <- design@r / (1 + design@r)^2
 
-            # Step 2
-            var_hat <- nuisance^2 / (n1 - 1) * (v1 + (z1 + sqrt(n1 * alloc) * Delta_star / nuisance)^2)
+  # Step 1
+  z1 <- stats::rnorm(n = iters, mean = 0, sd = 1)
+  v1 <- stats::rchisq(n = iters, df = n1 - 2)
 
-            # Step 3
-            if (recalculation == FALSE) {
-              n <- n1
-              } else {
-                n_recalc <- 1 / alloc * (stats::qnorm(1 - design@alpha) + stats::qnorm(1 - design@beta))^2 /
-                  (design@delta - design@delta_NI)^2 * var_hat
-                n        <- sapply(n_recalc, function(m) min(design@n_max, max(ceiling(m), n1)))
-              }
-            n2 <- n - n1
+  # Step 2
+  var_hat <- nuisance^2 / (n1 - 1) * (v1 + (z1 + sqrt(n1 * alloc) * Delta_star / nuisance)^2)
 
-            f <- function(i) {
-              # Step 4
-              if(n2[i] == 0) {
-                test_statistic <- (z1[i] + sqrt(n1 * alloc) * (Delta_star - design@delta_NI) / nuisance) / sqrt(v1[i] / (n1 - 2))
-                } else {
-                  # Step 5
-                  z2 <- stats::rnorm(n = 1, mean = 0, sd = 1)
-                  w2 <- stats::rchisq(n = 1, df = n2[i] - 1)
-                  v2 <- w2 + (sqrt(n2[i] / n[i]) * z1[i] - sqrt(n1 / n[i]) * z2)^2
+  # Step 3
+  if (recalculation == FALSE) {
+    n <- n1
+    } else {
+      n_recalc <- 1 / alloc * (stats::qnorm(1 - design@alpha) + stats::qnorm(1 - design@beta))^2 /
+        (design@delta - design@delta_NI)^2 * var_hat
+      n        <- sapply(n_recalc, function(m) min(design@n_max, max(ceiling(m), n1)))
+    }
 
-                  test_statistic <-
-                    (sqrt(n1 / n[i]) * z1[i] + sqrt(n2[i] / n[i]) * z2 + sqrt(n[i] * alloc) *
-                       (Delta_star - design@delta_NI) / nuisance) / sqrt((v1[i] + v2) / (n[i] - 2))
-                  }
-              return(test_statistic)
-            }
+  n2 <- n - n1
 
-            test_statistic <- sapply(seq(1, iters, 1), f)
+  f <- function(i) {
+    # Step 4
+    if(n2[i] == 0) {
+      test_statistic <- (z1[i] + sqrt(n1 * alloc) * (Delta_star - design@delta_NI) / nuisance) / sqrt(v1[i] / (n1 - 2))
+      } else {
+        # Step 5
+        z2 <- stats::rnorm(n = 1, mean = 0, sd = 1)
+        w2 <- stats::rchisq(n = 1, df = n2[i] - 1)
+        v2 <- w2 + (sqrt(n2[i] / n[i]) * z1[i] - sqrt(n1 / n[i]) * z2)^2
 
-            critical_value <- stats::qt(1 - design@alpha, df = n - 2)
-            reject <- ifelse(test_statistic >= critical_value, 1, 0)
+        test_statistic <-
+          (sqrt(n1 / n[i]) * z1[i] + sqrt(n2[i] / n[i]) * z2 + sqrt(n[i] * alloc) *
+             (Delta_star - design@delta_NI) / nuisance) / sqrt((v1[i] + v2) / (n[i] - 2))
+        }
+    return(test_statistic)
+    }
 
-            return(list(
-              "rejection_probability" = mean(reject),
+  test_statistic <- sapply(seq(1, iters, 1), f)
+  critical_value <- stats::qt(1 - design@alpha, df = n - 2)
+  reject         <- ifelse(test_statistic >= critical_value, 1, 0)
+
+  return(list("rejection_probability" = mean(reject),
               "sample_sizes" = n
-              ))
+  ))
 }
 
 
