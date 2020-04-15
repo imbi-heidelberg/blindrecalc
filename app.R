e130a5d7d3bda04842c8b2e42f2081c9dfd468a5
MatheMax <pilz@imbi.uni-heidelberg.de>
2020-04-15 11:14:01 +0200

    add chi square test

--- app.R
+++ app.R
@@ -8,152 +8,270 @@ if (!require("blindrecalc"))
 library(shiny)
 library(blindrecalc)
 
-ui <- fluidPage(
-   titlePanel("Blinded Sample Size Recalculation With the Package blindrecalc"),
+max_sliders <- 10
 
-   fluidRow(
-
-     column(3,
-            sliderInput("alpha",
-                        "Maximal Type One Error Rate",
-                               min = .01,
-                               max = .1,
-                               step = .005,
-                               value = .025),
-                   sliderInput("beta",
-                               "Maximal Type Two Error Rate",
-                               min = .05,
-                               max = .4,
-                               step = .01,
-                               value = .2),
-                   sliderInput("r",
-                               "Allocation Ratio",
-                               min = 1/5,
-                               max = 5,
-                               step = .1,
-                               value = 1),
-                   sliderInput("delta",
-                               "Alternative Effect Size",
-                               min = .05,
-                               max = 1,
-                               value = .5,
-                               step = .01),
-                   sliderInput("delta_NI",
-                               "Non-Inferiority Margin",
-                               min = 0,
-                               max = 1,
-                               value = 0,
-                               step = .01),
-                   sliderInput("n_max",
-                               "Maximal Sample Size",
-                               min = 10,
-                               max = 10000,
-                               value = 10000,
-                               step = 2)
-     ),
-     column(3,
-                   sliderInput("n1",
-                               "Sample Size of Internal Pilot Study",
-                               min = 1,
-                               max = 1000,
-                               value = 10,
-                               step = 1),
-                   sliderInput("nuisance",
-                               "Assumed Variance",
-                               min = 0.5,
-                               max = 20,
-                               value = 1,
-                               step = .5),
-                   sliderInput("iters",
-                               "Number of Simulation Iterations",
-                               min = 1000,
-                               max = 100000,
-                               value = 10000,
-                               step = 1000),
-                   submitButton("Compute!")
-     ),
-     column(6,
-            "sample size distribution",
-            plotOutput(outputId = "dist_plot"),
-            verbatimTextOutput("toer"),
-            verbatimTextOutput("pow"),
-            verbatimTextOutput("n_fix"))
-
-
-      ),
-
-   p("This shiny app was developed by:"),
-   a(href="https://www.klinikum.uni-heidelberg.de/medizinische-biometrie/wir-
-ueber-uns/wir-ueber-uns", img(src="Logo-IMBI_ENGL.jpg", height=100))
-)
+ui <- navbarPage("blindrecalc",
+                 tabPanel("t-test",
+                          fluidPage(
+                            titlePanel("Blinded Sample Size Recalculation for Student's t-Test"),
+                            fluidRow(
+                              column(3,
+                                     sliderInput("t_alpha",
+                                                 "Maximal Type One Error Rate",
+                                                 min = .01,
+                                                 max = .1,
+                                                 step = .005,
+                                                 value = .025),
+                                     sliderInput("t_beta",
+                                                 "Maximal Type Two Error Rate",
+                                                 min = .05,
+                                                 max = .4,
+                                                 step = .01,
+                                                 value = .2),
+                                     sliderInput("t_r",
+                                                 "Allocation Ratio",
+                                                 min = 1/5,
+                                                 max = 5,
+                                                 step = .1,
+                                                 value = 1),
+                                     sliderInput("t_delta",
+                                                 "Alternative Effect Size",
+                                                 min = .05,
+                                                 max = 1,
+                                                 value = .5,
+                                                 step = .01),
+                                     sliderInput("t_delta_NI",
+                                                 "Non-Inferiority Margin",
+                                                 min = 0,
+                                                 max = 1,
+                                                 value = 0,
+                                                 step = .01),
+                                     sliderInput("t_n_max",
+                                                 "Maximal Sample Size",
+                                                 min = 10,
+                                                 max = 10000,
+                                                 value = 10000,
+                                                 step = 2)
+                                     ),
+                              column(3,
+                                     sliderInput("t_n1",
+                                                 "Sample Size of Internal Pilot Study",
+                                                 min = 1,
+                                                 max = 1000,
+                                                 value = 10,
+                                                 step = 1),
+                                     sliderInput("t_nuisance",
+                                                 "Assumed Variance",
+                                                 min = 0.5,
+                                                 max = 20,
+                                                 value = 1,
+                                                 step = .5),
+                                     sliderInput("t_iters",
+                                                 "Number of Simulation Iterations",
+                                                 min = 1000,
+                                                 max = 100000,
+                                                 value = 10000,
+                                                 step = 1000),
+                                     submitButton("Compute!")
+                                     ),
+                              column(6,
+                                     "sample size distribution",
+                                     plotOutput(outputId = "t_dist_plot"),
+                                     verbatimTextOutput("t_toer"),
+                                     verbatimTextOutput("t_pow"),
+                                     verbatimTextOutput("t_n_fix"))
+                              ),
+                            p("This shiny app was developed by:"),
+                            a(href="https://www.klinikum.uni-heidelberg.de/medizinische-biometrie/wir-
+                              ueber-uns/wir-ueber-uns", img(src="Logo-IMBI_ENGL.jpg", height=100))
+                            )
+                          ),
+                 tabPanel("Chi-Square-test",
+                          fluidPage(
+                            titlePanel("Blinded Sample Size Recalculation for the Chi-Square-Test"),
+                            fluidRow(
+                              column(3,
+                                     sliderInput("cs_alpha",
+                                                 "Maximal Type One Error Rate",
+                                                 min = .01,
+                                                 max = .1,
+                                                 step = .005,
+                                                 value = .025),
+                                     sliderInput("cs_beta",
+                                                 "Maximal Type Two Error Rate",
+                                                 min = .05,
+                                                 max = .4,
+                                                 step = .01,
+                                                 value = .2),
+                                     sliderInput("cs_r",
+                                                 "Allocation Ratio",
+                                                 min = 1/5,
+                                                 max = 5,
+                                                 step = .1,
+                                                 value = 1),
+                                     sliderInput("cs_delta",
+                                                 "Alternative Effect Size",
+                                                 min = .05,
+                                                 max = 1,
+                                                 value = .5,
+                                                 step = .01),
+                                     sliderInput("cs_n_max",
+                                                 "Maximal Sample Size",
+                                                 min = 10,
+                                                 max = 10000,
+                                                 value = 10000,
+                                                 step = 2)
+                              ),
+                              column(3,
+                                     sliderInput("cs_n1",
+                                                 "Sample Size of Internal Pilot Study",
+                                                 min = 1,
+                                                 max = 1000,
+                                                 value = 10,
+                                                 step = 1),
+                                     sliderInput("cs_nuisance",
+                                                 "Assumed Overall Response Rate",
+                                                 min = 0.05,
+                                                 max = 0.95,
+                                                 value = 0.5,
+                                                 step = .05),
+                                     submitButton("Compute!")
+                              ),
+                              column(6,
+                                     "sample size distribution",
+                                     plotOutput(outputId = "cs_dist_plot"),
+                                     verbatimTextOutput("cs_toer"),
+                                     verbatimTextOutput("cs_pow"),
+                                     verbatimTextOutput("cs_n_fix"))
+                            ),
+                            p("This shiny app was developed by:"),
+                            a(href="https://www.klinikum.uni-heidelberg.de/medizinische-biometrie/wir-
+                              ueber-uns/wir-ueber-uns", img(src="Logo-IMBI_ENGL.jpg", height=100))
+                          )
+                 )
+                 )
 
 
 
 
 server <- function(input, output) {
 
+  # outpur for t-test
 
+  output$t_dist_plot <- renderPlot({
 
-  output$dist_plot <- renderPlot({
-
-    d <- blindrecalc::setupStudent(alpha = input$alpha,
-                                   beta = input$beta,
-                                   r = input$r,
-                                   delta = input$delta,
-                                   delta_NI = input$delta_NI,
+    d <- blindrecalc::setupStudent(alpha = input$t_alpha,
+                                   beta = input$t_beta,
+                                   r = input$t_r,
+                                   delta = input$t_delta,
+                                   delta_NI = input$t_delta_NI,
                                    alternative = "greater",
-                                   n_max = input$n_max)
+                                   n_max = input$t_n_max)
 
 
     withProgress(message = "Computing characteristics", {
 
-    return(blindrecalc::n_dist(d, input$n1, input$nuisance, summary = FALSE,
-                               plot = TRUE, iters = input$iters, seed = 2020))
+    return(blindrecalc::n_dist(d, input$t_n1, input$t_nuisance, summary = FALSE,
+                               plot = TRUE, iters = input$t_iters, seed = 2020))
     })
 
   })
 
-  output$toer <- renderText({
-    d <- blindrecalc::setupStudent(alpha = input$alpha,
-                                   beta = input$beta,
-                                   r = input$r,
-                                   delta = input$delta,
-                                   delta_NI = input$delta_NI,
+  output$t_toer <- renderText({
+    d <- blindrecalc::setupStudent(alpha = input$t_alpha,
+                                   beta = input$t_beta,
+                                   r = input$t_r,
+                                   delta = input$t_delta,
+                                   delta_NI = input$t_delta_NI,
                                    alternative = "greater",
-                                   n_max = input$n_max)
+                                   n_max = input$t_n_max)
 
-    return(paste("type I error rate:", blindrecalc::toer(d, input$n1, input$nuisance,
+    return(paste("type I error rate:", blindrecalc::toer(d, input$t_n1, input$t_nuisance,
                                                          recalculation = TRUE,
-                                                         iters = input$iters, seed = 2020)))
+                                                         iters = input$t_iters, seed = 2020)))
   })
 
-  output$pow <- renderText({
-    d <- blindrecalc::setupStudent(alpha = input$alpha,
-                                   beta = input$beta,
-                                   r = input$r,
-                                   delta = input$delta,
-                                   delta_NI = input$delta_NI,
+  output$t_pow <- renderText({
+    d <- blindrecalc::setupStudent(alpha = input$t_alpha,
+                                   beta = input$t_beta,
+                                   r = input$t_r,
+                                   delta = input$t_delta,
+                                   delta_NI = input$t_delta_NI,
                                    alternative = "greater",
-                                   n_max = input$n_max)
+                                   n_max = input$t_n_max)
 
-    return(paste("power:", blindrecalc::pow(d, input$n1, input$nuisance,
+    return(paste("power:", blindrecalc::pow(d, input$t_n1, input$t_nuisance,
                                                          recalculation = TRUE,
-                                                         iters = input$iters, seed = 2020)))
+                                                         iters = input$t_iters, seed = 2020)))
   })
 
 
-  output$n_fix <- renderText({
-    d <- blindrecalc::setupStudent(alpha = input$alpha,
-                                   beta = input$beta,
-                                   r = input$r,
-                                   delta = input$delta,
-                                   delta_NI = input$delta_NI,
+  output$t_n_fix <- renderText({
+    d <- blindrecalc::setupStudent(alpha = input$t_alpha,
+                                   beta = input$t_beta,
+                                   r = input$t_r,
+                                   delta = input$t_delta,
+                                   delta_NI = input$t_delta_NI,
                                    alternative = "greater",
-                                   n_max = input$n_max)
+                                   n_max = input$t_n_max)
 
-    return(paste("fixed sample size:", ceiling(blindrecalc::n_fix(d, input$nuisance))))
+    return(paste("fixed sample size:", ceiling(blindrecalc::n_fix(d, input$t_nuisance))))
   })
 
+
+  # output for chi-square test
+
+  output$cs_dist_plot <- renderPlot({
+
+    d <- blindrecalc::setupChiSquare(alpha = input$cs_alpha,
+                                     beta = input$cs_beta,
+                                     r = input$cs_r,
+                                     delta = input$cs_delta,
+                                     n_max = input$cs_n_max)
+
+
+    withProgress(message = "Computing characteristics", {
+
+      return(blindrecalc::n_dist(d, input$cs_n1, input$cs_nuisance, summary = FALSE,
+                                 plot = TRUE))
+    })
+
+  })
+
+  output$cs_toer <- renderText({
+    d <- blindrecalc::setupChiSquare(alpha = input$cs_alpha,
+                                     beta = input$cs_beta,
+                                     r = input$cs_r,
+                                     delta = input$cs_delta,
+                                     n_max = input$cs_n_max)
+
+    return(paste("type I error rate:", blindrecalc::toer(d, input$cs_n1, input$cs_nuisance,
+                                                         recalculation = TRUE,
+                                                         iters = input$cs_iters, seed = 2020)))
+  })
+
+  output$cs_pow <- renderText({
+    d <- blindrecalc::setupChiSquare(alpha = input$cs_alpha,
+                                     beta = input$cs_beta,
+                                     r = input$cs_r,
+                                     delta = input$cs_delta,
+                                     n_max = input$cs_n_max)
+
+    return(paste("power:", blindrecalc::pow(d, input$cs_n1, input$cs_nuisance,
+                                            recalculation = TRUE,
+                                            iters = input$cs_iters, seed = 2020)))
+  })
+
+
+  output$cs_n_fix <- renderText({
+    d <- blindrecalc::setupChiSquare(alpha = input$cs_alpha,
+                                     beta = input$cs_beta,
+                                     r = input$cs_r,
+                                     delta = input$cs_delta,
+                                     n_max = input$cs_n_max)
+
+    return(paste("fixed sample size:", ceiling(blindrecalc::n_fix(d, input$cs_nuisance))))
+  })
 
 
 }
