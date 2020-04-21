56358a6153101b6bdb15eed7fd9b8b182102c759
MatheMax <pilz@imbi.uni-heidelberg.de>
2020-04-21 16:09:38 +0200

    test farrington manning

--- tests/testthat/test_farringtonmanning_pow.R
+++ tests/testthat/test_farringtonmanning_pow.R
@@ -2,27 +2,27 @@ context("test pow for the Farrington-Manning test")
 
 test_that("pow works for the fixed design (Friede et al. 2007)", {
   design1 <- setupFarringtonManning(alpha = 0.025, beta = 0.2,
-    r = 1, delta = 0, delta_NI = 0.1)
+                                    r = 1, delta = 0, delta_NI = 0.1)
   pow1 <- pow(design1, n1 = 658, nuisance = 0.3, recalculation = FALSE)
   pow2 <- pow(design1, n1 = 780, nuisance = 0.5, recalculation = FALSE)
 
   design2 <- setupFarringtonManning(alpha = 0.025, beta = 0.2,
-    r = 2, delta = 0, delta_NI = 0.1)
+                                    r = 2, delta = 0, delta_NI = 0.1)
   n1d2 <- n_fix(design2, nuisance = 0.4, rounded = FALSE)
   pow3 <- pow(design2, n1 = n1d2, nuisance = 0.4,
-    allocation = "approximate", recalculation = FALSE)
+              allocation = "approximate", recalculation = FALSE)
   n2d2 <- n_fix(design2, nuisance = 0.6, rounded = FALSE)
   pow4 <- pow(design2, n1 = n2d2, nuisance = 0.6,
-    allocation = "approximate", recalculation = FALSE)
+              allocation = "approximate", recalculation = FALSE)
 
   design3 <- setupFarringtonManning(alpha = 0.025, beta = 0.2,
-    r = 3, delta = 0, delta_NI = 0.1)
+                                    r = 3, delta = 0, delta_NI = 0.1)
   n1d3 <- n_fix(design3, nuisance = 0.3, rounded = FALSE)
   pow5 <- pow(design3, n1 = n1d3, nuisance = 0.3,
-    allocation = "approximate", recalculation = FALSE)
+              allocation = "approximate", recalculation = FALSE)
   n2d3 <- n_fix(design3, nuisance = 0.4, rounded = FALSE)
   pow6 <- pow(design3, n1 = n2d3, nuisance = 0.4,
-    allocation = "approximate", recalculation = FALSE)
+              allocation = "approximate", recalculation = FALSE)
 
 
   expect_equal(pow1, 0.8005762, tolerance = 1e-7)
@@ -32,3 +32,22 @@ test_that("pow works for the fixed design (Friede et al. 2007)", {
   expect_equal(pow5, 0.8009629, tolerance = 1e-7)
   expect_equal(pow6, 0.8026954, tolerance = 1e-7)
 })
+
+
+test_that("errors are defined correctly", {
+  d1 <- setupFarringtonManning(alpha = 0.025, beta = 0.2, r = 2,
+                               delta = 0, delta_NI = 0.1)
+  expect_error(pow(d1, 1.1, TRUE))
+
+  d2 <- setupFarringtonManning(alpha = 0.025, beta = 0.2, r = 2,
+                               delta = 0, delta_NI = 0.1, n_max = 301)
+  expect_error(pow(d2, 21, 0.5, TRUE, "exact"))
+  d2@n_max <- 300
+  expect_error(pow(d2, 20, 0.5, TRUE, "exact"))
+
+  expect_error(pow(d2, d2@n_max + 1, 0.7, TRUE, "approximate"))
+
+  expect_error(pow(d2, c(20, 30), c(0.6, 0.7), TRUE, "approximate"))
+
+})
+
--- tests/testthat/test_farringtonmanning_toer.R
+++ tests/testthat/test_farringtonmanning_toer.R
@@ -1,10 +1,29 @@
 context("test toer for the Farrington-Manning test")
 
 test_that("toer gives same values as pow for delta = 0", {
-  design1 <- setupFarringtonManning(alpha = 0.025, beta = 0.2, r = 1,
-    delta = 0, delta_NI = 0, n_max = 250)
+  design1 <- suppressWarnings(setupFarringtonManning(alpha = 0.025, beta = 0.2,
+                                                     r = 1, delta = 0,
+                                                     delta_NI = 0, n_max = 250))
   toer1 <- toer(design1, n1 = 80, nuisance = 0.4, recalculation = FALSE)
   power1 <- pow(design1, n1 = 80, nuisance = 0.4, recalculation = FALSE)
 
   expect_equal(toer1, power1)
+})
+
+
+test_that("errors are defined correctly", {
+  d1 <- setupFarringtonManning(alpha = 0.025, beta = 0.2, r = 2,
+                               delta = 0, delta_NI = 0.1)
+  expect_error(toer(d1, 1.1, TRUE))
+
+  d2 <- setupFarringtonManning(alpha = 0.025, beta = 0.2, r = 2,
+                               delta = 0, delta_NI = 0.1, n_max = 301)
+  expect_error(toer(d2, 21, 0.5, TRUE, "exact"))
+  d2@n_max <- 300
+  expect_error(toer(d2, 20, 0.5, TRUE, "exact"))
+
+  expect_error(toer(d2, d2@n_max + 1, 0.7, TRUE, "approximate"))
+
+  expect_error(toer(d2, c(20, 30), c(0.6, 0.7), TRUE, "approximate"))
+
 })
