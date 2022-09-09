289f70e682a56246d86cffbbb56a7105ba795b66
Maximilian Pilz <maximilian.pilz@itwm.fraunhofer.de>
2022-09-09 09:06:53 +0200

    add R Journal reference

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# blindrecalc 0.1.4
+
+* fix documentation to pass CRAN check
+
+* add reference to R Journal publication
--- R/blindrecalc.R
+++ R/blindrecalc.R
@@ -9,6 +9,9 @@
 #' For superiority trials with binary endpoint, the chi^2-test is implemented.
 #' The Farrington Manning test covers non-inferiority trials with binary endpoint.
 #'
+#' A detailed description of the package usage can be found
+#' \href{https://doi.org/10.32614/RJ-2022-001}{here}.
+#'
 #' @useDynLib blindrecalc
 #' @import methods
 #' @importFrom Rcpp sourceCpp
